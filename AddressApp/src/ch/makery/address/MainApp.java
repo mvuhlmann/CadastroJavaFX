2e2d45c795bbe3968b470b4d482081190e430966
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-04 23:06:31 -0300

    Metodo showPersonOverview() modificado

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -3,6 +3,7 @@ package ch.makery.address;
 import java.io.IOException;
 
 import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -82,15 +83,23 @@ public class MainApp extends Application {
     /**
      * Mostra o person overview dentro do root layout.
      */
+    /**
+     * Mostra a person overview dentro do root layout.
+     */
     public void showPersonOverview() {
         try {
-            // Carrega o person overview.
+            // Carrega a person overview.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
-            // Define o person overview dentro do root layout.
+            // Define a person overview no centro do root layout.
             rootLayout.setCenter(personOverview);
+
+            // Dá ao controlador acesso à the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
