016fff9be7d6498bf19514910d3dc8754469aea2
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-04 23:43:33 -0300

    Metodo handleDeletePerson adicionado na classe person

--- AddressApp/src/ch/makery/address/view/PersonOverviewController.java
+++ AddressApp/src/ch/makery/address/view/PersonOverviewController.java
@@ -98,4 +98,12 @@ public class PersonOverviewController {
             birthdayLabel.setText("");
         }
     }
+    /**
+     * Chamado quando o usuário clica no botão delete.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        personTable.getItems().remove(selectedIndex);
+    }
 }
